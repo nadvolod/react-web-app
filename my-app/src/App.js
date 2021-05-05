39da4c4f6fdcdd5ef4d7a945076ca38129bb01a6
nadvolod <nadvolod@gmail.com>
2021-05-05 17:58:14 -0400

    add another test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ultimateqa.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Testing
         </a>
       </header>
     </div>
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn testing link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn testing/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('should have correct url', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn testing/i);
+  expect(linkElement.href).toContain('ultimateqa.com')
+})

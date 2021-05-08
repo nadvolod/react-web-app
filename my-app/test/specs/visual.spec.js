beaf5b272ba8f1c3365949aeb98b6ec3a96dab01
nadvolod <nadvolod@gmail.com>
2021-05-08 09:55:42 -0400

    start from scratch

--- my-app/test/specs/visual.spec.js
+++
@@ -1,8 +0,0 @@
-describe('My React application', () => {
-    it('should look correct', () => {
-        browser.url(``);
-        browser.execute('/*@visual.init*/', 'My React App');
-        browser.execute('/*@visual.snapshot*/', 'Home Page');
-    });
-});
-

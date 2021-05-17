cc15af5ce3acd38a25efdabf98830bdd1d71e9a4
nadvolod <nadvolod@gmail.com>
2021-05-17 09:18:39 -0400

    change name of ci to old

--- my-app/test/specs/visual.spec.js
+++ my-app/test/specs/visual.spec.js
@@ -1,5 +1,4 @@
-describe('My React application', () => {
-    
+describe('My React application', () => {   
     it('should look correct', () => {
         browser.url('');
         browser.execute('/*@visual.init*/', 'My React App');

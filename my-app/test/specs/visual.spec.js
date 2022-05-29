8b783acacc56063be1201667ca88e6bdee2d0cec
ztopcuoglu <zfrtopcuoglu@gmail.com>
2022-05-29 14:30:26 +0300

    first

--- my-app/test/specs/visual.spec.js
+++
@@ -1,9 +0,0 @@
-describe('My React application', () => {   
-    it('should look correct', () => {
-        browser.url('');
-        browser.execute('/*@visual.init*/', 'My React App');
-        browser.execute('/*@visual.snapshot*/', 'Home Page');
-        const result = browser.execute('/*@visual.end*/');
-        expect(result.message).toBeNull();
-    });
-});
